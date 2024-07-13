8d6b916d1a86efabc50404c65702bd17295faa96
Hassan Boss <Hassan_siala@hotmail.com>
2024-07-13 15:23:39 +0200

    Done

---
+++ src/assets/index.js
@@ -0,0 +1,54 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+
+import java from "./company/java.png";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import android from "./tech/android.png"
+import vue from "./tech/vue.png"
+import flutter from "./tech/flutter.png"
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  tailwind,
+  threejs,
+  java,
+  carrent,
+  jobit,
+  tripguide,
+  android,
+  vue,
+  flutter,
+};
---
+++ src/components/About.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+      >
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        I am a passionate and ambitious Java Developer with a vocational degree from Grit Academy in Malmö. I have a broad skill set ranging from basic web development to advanced Java programming. My technical expertise includes advanced knowledge in Java, JavaScript (React JS, Vue JS, Angular), HTML/CSS (including Tailwind CSS), and Android development, with foundational skills in Flutter and React Native. I also have experience with databases such as MySQL and Firebase, and development environments like Docker.
+
+In addition to my technical skills, I am well-versed in agile methodologies and project management. I have successfully led and participated in various projects, and have experience working as a consultant. I am highly motivated and eager to tackle new challenges. With my background and drive, I am confident that I can be a valuable asset to your company.
+
+I am a quick learner who loves programming and coding. I prioritize close collaboration with my clients to create and deliver efficient, scalable, and user-friendly projects.
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
