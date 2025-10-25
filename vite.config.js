297c559a6efc63dd559d795550476ea9ad437026
John Doe <johndoe@email.com>
2025-10-25 22:18:40 +0200

    Work in progress

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/HassansPortfolio",
-  plugins: [react()],
+  plugins: [react(), reactRefresh()],
 });
